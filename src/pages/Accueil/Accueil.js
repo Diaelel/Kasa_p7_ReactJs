481efe6e303121e87539314f7790aa029d945735
Diaa Elazzouzi <elazzouzi.diaa@gmail.com>
2022-11-29 23:46:16 +0100

    Routes

---
+++ src/pages/Accueil/Accueil.js
@@ -0,0 +1,13 @@
+// import React from "react";
+// import "./Accueil.css";
+// import Banniere from "../../components/Banniere/Banniere";
+// import banniereAccueil from "../assets/img/Pages/Accuel/Banniere.png";
+// import { NavLink } from "react-router-dom";
+// import Tuile from "../../components/Tuile/Tuile";
+// import ListeLogements from "../..assets/api/logements.json";
+
+// function Accueil () {
+//     return {
+
+//     }
+// }
